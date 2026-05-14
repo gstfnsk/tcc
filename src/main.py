9df25495c7158d2b3f48870bfe7b0363b3c18818
gstfnsk <giulia.chimini@gmail.com>
2026-05-14 09:40:29 -0300

    add more prints

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 from src.core import Pipeline
 from src.chunkers.simple_fixed_recursive_chunker import SimpleChunker
-# from src.embedders.nomic_moe import NomicEmbedder
-from src.embedders.all_minilm import AllMinilmEmbedder
+from src.embedders.nomic_moe import NomicEmbedder
+# from src.embedders.all_minilm import AllMinilmEmbedder
 from src.store.chroma import ChromaStore
 from src.generator.oss_generator import gpt_oss_Generator
 
 pipeline = Pipeline(
     chunker=SimpleChunker(chunk_size=300, chunk_overlap=50),
-    embedder=AllMinilmEmbedder(),
+    embedder=NomicEmbedder(),
     store=ChromaStore(path="./chroma_db", collection_name="test"), 
     generator=gpt_oss_Generator()
 )
@@ -24,5 +24,8 @@ generation = pipeline.generate_answer(query, retrieved_chunks=retrieved_chunks)
 #     print(r.chunk.text)
 #     print(r.score)
 
-print("Resposta gerada:")
-print(generation)
+# print("Resposta crua gerada:")
+# print(generation)
+
+print("Resposta final:")
+print(generation.message.content)
